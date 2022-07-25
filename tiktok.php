b137d4e311b00d490800ee4e9a6b42147d38b8d4
Jakhongir Ganiev <63680003+GaniyevUz@users.noreply.github.com>
2022-07-25 13:00:56 +0500

    Update tiktok.php

--- tiktok.php
+++ tiktok.php
@@ -1,20 +1,17 @@
-<?php
+class TikTok
+{
+    private $links = [];
 
-$url = $_GET["url"];
-$type = $_GET["type"];
-$get = json_decode(file_get_contents("https://tikdown.org/getAjax?url=" . urlencode($url) . "&_token=t4JdZ9etWtvCbI68c77THtVyYJEtEp4DrGg2Af8W"), true);
-print_r($get);
-die();
-$links = [];
+    public function __construct($url)
+    {
+        $get = json_decode(file_get_contents("https://tikdown.org/getAjax?url=" . urlencode($url) . "&_token=t4JdZ9etWtvCbI68c77THtVyYJEtEp4DrGg2Af8W"), true);
+        $ex = explode('href="', $get['html']);
+        array_push($this->links, explode('" name="download"', $ex[1])[0]);
+        array_push($this->links, explode('" name="download"', $ex[2])[0]);
+    }
 
-$ex = explode('href="', $get['html']);
-array_push($links,  explode('" name="download"', $ex[1])[0]);
-array_push($links,  explode('" name="download"', $ex[2])[0]);
-
-// header("content-type: application/json");
-// echo json_encode($links, JSON_PRETTY_PRINT);
-if($type == 1){
-    header("location: ".$links[0]);
-}else { 
-    header("location: ".$links[1]);
+    public function getLinks()
+    {
+        return ['result' => true, 'developer' => 'Jakhongir Ganiev', 'links' => $this->links];
+    }
 }
